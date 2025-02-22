111e91744c52f3b057dacd3f4dfdd9f81b0eccdf
Daniel Perez Alvarez <daniel@unindented.org>
2025-02-22 15:07:53 -0800

    feat: create Astro project

---
+++ website/src/loaders/utils.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2025 Daniel Perez Alvarez
+ *
+ * This file is part of Static Feeds.
+ *
+ * Static Feeds is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Static Feeds is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Static Feeds. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import path from "node:path";
+
+/**
+ * Convert a platform path to a POSIX path.
+ */
+export function posixifyPath(filePath: string): string {
+  return filePath.split(path.sep).join("/");
+}
+
+/**
+ * Unlike `path.posix.relative`, this function will accept a platform path and
+ * return a POSIX path.
+ */
+export function posixRelative(from: string, to: string): string {
+  return posixifyPath(path.relative(from, to));
+}
